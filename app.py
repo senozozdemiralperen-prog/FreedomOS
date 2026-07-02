af26d56e98e223b155e18f125e2e6d7071221e48
senozozdemiralperen-prog <senozozdemiralperen@gmail.com>
2026-07-02 17:58:09 +0300

    Update app.py

--- app.py
+++ app.py
@@ -109,7 +109,7 @@ page = st.sidebar.radio(
     "Gitmek İstediğiniz Sayfa:",
     [
         "🏠 Genel Durum & Özet Paneli", 
-        "📊 Gelir / Detaylı Gider Kaydı", 
+        "📊 Gelir / Gider Detaylı Kaydı", 
         "💎 Detaylı Mal Varlığı Envanteri",
         "📈 Varlık & Portföy Rebalancing", 
         "🔮 Maaşlı Çalışmadan Kurtulma Motoru"
